11757efb10fd7fbb376615f8be82d03b51fa3a48
Kaushik <kausriniv@gmail.com>
2022-11-29 19:13:29 +0530

    Computing time taken by prover and verifier

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 mod prover;
+use std::time::Instant;
+
 use prover::{prove_work, verify_work};
 
 fn main() {
+    let now = Instant::now();
     let (result, proof) = prove_work();
+    let elapsed = now.elapsed();
+    println!("Computing fibonacci and proof took: {:.2?}", elapsed);
+
+    let then = Instant::now();
     verify_work(result, proof);
+    let fin = then.elapsed();
+    println!("Proof verification took: {:.2?}", fin);
 }
